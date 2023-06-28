da4281446ffb00ce882d8bbbcfde573d64ddf1f8
Ivan Solovev <ivan@solovev.one>
2023-06-29 00:19:27 +0300

    change site

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,5 +1,8 @@
-const data = require("./data.json");
+const fs = require('node:fs')
+const YAML = require('yaml')
+
+const file = fs.readFileSync('./data.yml', 'utf8');
 
 module.exports = {
-  locals: data
+  locals: YAML.parse(file),
 };
